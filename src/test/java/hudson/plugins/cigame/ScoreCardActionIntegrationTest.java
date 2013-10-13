d83d935d79d2582ef24988401cc1dc2bd543d0df
octavian-h <h.octavian@gmail.com>
2013-10-13 20:10:38 +0300

    update versions

--- src/test/java/hudson/plugins/cigame/ScoreCardActionIntegrationTest.java
+++ src/test/java/hudson/plugins/cigame/ScoreCardActionIntegrationTest.java
@@ -1,25 +1,24 @@
 package hudson.plugins.cigame;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-
+import com.gargoylesoftware.htmlunit.html.HtmlTable;
 import org.jvnet.hudson.test.HudsonTestCase;
 import org.jvnet.hudson.test.recipes.LocalData;
 
-import com.gargoylesoftware.htmlunit.html.HtmlTable;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class ScoreCardActionIntegrationTest extends HudsonTestCase {
 
     @LocalData
     public void testThatUsernameWithDifferentCasingIsDisplayedAsOne() throws Exception {
         hudson.getDescriptorByType(GameDescriptor.class).setNamesAreCaseSensitive(false);
-        HtmlTable table = (HtmlTable) new WebClient().goTo("job/multiple-culprits/4/cigame/").getHtmlElementById("game.culprits");
+        HtmlTable table = (HtmlTable) new WebClient().goTo("job/multiple-culprits/4/ci-game/").getHtmlElementById("game.culprits");
         assertThat(table.getRowCount(), is(2));
     }
     
     @LocalData
     public void testThatUsernameWithDifferentCasingIsNotDisplayedAsOne() throws Exception {
-        HtmlTable table = (HtmlTable) new WebClient().goTo("job/multiple-culprits/4/cigame/").getHtmlElementById("game.culprits");
+        HtmlTable table = (HtmlTable) new WebClient().goTo("job/multiple-culprits/4/ci-game/").getHtmlElementById("game.culprits");
         assertThat(table.getRowCount(), is(3));        
     }
 }
